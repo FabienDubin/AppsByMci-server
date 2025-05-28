2562ef8f7ac5743abc592050312df1febdeac455
Fab Dubin <fabien.dubin@gmail.com>
2025-05-28 22:58:13 +0200

    Deployind server

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ const cookieParser = require("cookie-parser");
 // unless the request is made from the same domain, by default express wont accept POST requests
 const cors = require("cors");
 
-const allowedOrigins = process.env.ORIGIN.split(",");
+const allowedOrigins = (process.env.ORIGIN || "").split(",");
 
 // Middleware configuration
 module.exports = (app) => {
